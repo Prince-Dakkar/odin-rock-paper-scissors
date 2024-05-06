4e9bbe988ba3f32c7de72b98bd70e4b2275ef376
Bri <bri.bumgardner@gmail.com>
2024-05-06 00:33:55 -0500

    Completed the RPS Revisited.

--- scripts.js
+++ scripts.js
@@ -1,65 +1,73 @@
 let humanScore = 0, computerScore = 0;
+const p_btn = document.getElementById("paper");
+const s_btn = document.getElementById("scissors");
+const r_btn = document.getElementById("rock");
+const result_msg = document.querySelector("#taunt");
+const your_score = document.querySelector("#p_score_label");
+const comp_score = document.querySelector("#c_score_label");
+const win_msg = document.querySelector("#win-msg");
+let game_session_ongoing = true;
+const choice = ["Paper", "Scissors", "Rock"];
+
 
 function getComputerChoice() {
     let rand = Math.random();
     if (rand < .33333333333) {
-        return "0";
+        return 0;
     } else if (rand < .6666666666) {
-        return "1";
-    } else return "2";
+        return 1;
+    } else return 2;
 }
 
-function getHumanChoice() {
-    let choice = prompt("Enter your choice: ");
-    choice = choice.toLowerCase();
-    if (choice === "rock") {
-        return "2";
-    } else if (choice === "paper") {
-        return "0";
-    } else if (choice === "scissors") {
-        return "1";
-    } else {
-        alert("Please enter a valid answer: rock, paper or scissors?");
-        return getHumanChoice();
-    }
-}
-
-function playRound(hC, cC) {
-    if (hC === cC) {
-        // They are the same
-        console.log("You and the Computer tied!");
-    } else {
-        if (Math.abs(hC - cC) == 1) {
-            // Bc of how the sequ. P, S, R is set up, if the indices are sequential, the greater wins
-            if (hC > cC) {
-                console.log("You Won!");
-                humanScore++;
-            } else {
-                console.log("The Computer Won!");
-                computerScore++;
-            }
-        } else { 
-            // One is paper and the other rock
-            if (hC == 2) {
-                console.log("You Won!");
-                humanScore++;
-            }  else {
-                console.log("The Computer Won!");
-                computerScore++;
+function playRound(hC) {
+    if (game_session_ongoing) {
+        const cC = getComputerChoice();
+        if (hC === cC) {
+            // They are the same
+            result_msg.textContent = "You and the Computer tied This Round!";
+            result_msg.style.backgroundColor = "yellow";
+        } else {
+            if (Math.abs(hC - cC) == 1) {
+                // Bc of how the sequ. P, S, R is set up, if the indices are sequential, the greater wins
+                if (hC > cC) {
+                    result_msg.textContent = "You Won This Round! " + choice[hC] + " beats " + choice[cC] + "!";
+                    result_msg.style.backgroundColor = "lightgreen";
+                    humanScore++;
+                    your_score.textContent = humanScore;
+                } else {
+                    result_msg.textContent = "The Computer Won This Round! " + choice[cC] + " beats " + choice[hC] + "!";
+                    result_msg.style.backgroundColor = "red";
+                    computerScore++;
+                    comp_score.textContent = computerScore;
+                }
+            } else { 
+                // One is paper and the other rock
+                if (hC == 0) {
+                    result_msg.textContent = "You Won This Round! " + choice[hC] + " beats " + choice[cC] + "!";
+                    result_msg.style.backgroundColor = "lightgreen";
+                    humanScore++;
+                    your_score.textContent = humanScore;
+                }  else {
+                    result_msg.textContent = "The Computer Won This Round! " + choice[cC] + " beats " + choice[hC] + "!";
+                    result_msg.style.backgroundColor = "red";
+                    computerScore++;
+                    comp_score.textContent = computerScore;
+                }
             }
         }
-    }
+        if (humanScore >= 5) {
+            win_msg.textContent = "YOU WON THE GAME!!!! \r\nTo Play Again, Reload the Page";
+            game_session_ongoing = false;
+            
+        } 
+        if (computerScore >= 5) {
+            win_msg.textContent = "THE COMPUTER WON :( \r\n To Play Again, Reload the Page";
+            game_session_ongoing = false;
+            
+        }
+    }    
 }
 
-function playGame(){
-   for (let i = 0; i < 5; i++) {
-        let c = getComputerChoice();
-        let h = getHumanChoice();
-        playRound(h, c);
-    }
-    if (humanScore >= computerScore) {
-       alert("You won the game!");
-    } else {
-       alert("The Computer won the game, ooooooof!"); 
-    }
-}
+p_btn.addEventListener("click", () => playRound(0));
+s_btn.addEventListener("click", () => playRound(1));
+r_btn.addEventListener("click", () => playRound(2));
